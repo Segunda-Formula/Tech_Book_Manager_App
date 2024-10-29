23fca091093ad1fcd55fdc26a9ae0e19aec6a11a
LilBiscuit <miccichestefano20@gmail.com>
2024-10-29 13:03:21 +0100

    Creamos switch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,34 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
         System.out.println("\nGestor de Libros Técnicos de Programación\n 1. Añadir libro\n 2. Ver todos los libros\n 3. Eliminar libro\n 4. Cambiar repositorio\n 5. Salir\n Seleccione una opción: ");
 
+        int opcion;
+        switch (opcion){
+            case 1:
+                System.out.println("Añadir libro");
+                break;
 
+            case 2:
+                System.out.println("Ver todos los libros");
+                break;
 
+            case 3:
+                System.out.println("Eliminar libro");
+                break;
+
+            case 4:
+                System.out.println("Cambiar repositorio");
+
+            case 5:
+                System.out.println("Salir");
+                break;
+
+        }
 
     }
 }
